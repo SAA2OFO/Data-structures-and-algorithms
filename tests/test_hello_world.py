0148ec9431ba4f01a20cbfe78ed716bb96192795
Alessandro Salvato <external.alessandro.salvato@vhit-weifu.com>
2025-10-02 09:08:48 +0200

    [Pylint] - Minor fix

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,4 +1,6 @@
+# pylint: disable=missing-module-docstring
+
 from src.basics.hello_world import main
 
-def test_hello_world():
+def test_hello_world(): # pylint: disable=missing-function-docstring
     assert main() == "Hello, world!"
