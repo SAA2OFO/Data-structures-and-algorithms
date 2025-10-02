033f86b66eb4dcd0ada6d29e8eec7fcd71c321d8
Alessandro Salvato <external.alessandro.salvato@vhit-weifu.com>
2025-10-02 08:47:42 +0200

    [Basics] - Following Pylint guidelines and disabling it

--- src/basics/hello_world.py
+++ src/basics/hello_world.py
@@ -1,3 +1,13 @@
+# pylint: disable=missing-module-docstring
+
+"""
+    This module contains a simple Hello World example
+    for demonstrating basic Python structure.
+"""
+
+def main():
+    print("Hello, world!")
 
 if __name__ == "__main__":
-    print("Hello World")
+    main()
+
